3c32c23b5ec8b1ad3f92ccbd602f4adc8409a4df
Robert Sanchez <robert.sanchez@sophos.com>
2018-07-09 08:25:06 -0700

    Pull data from MongoDB

--- node/WFI_Server.js
+++ node/WFI_Server.js
@@ -1,9 +1,56 @@
 const express = require("express");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
 
-app.get("/:fname/:lname", function(){
+
+app.get("/user/:id", function(request,response){
+	console.log("users",request.params);
 	response.header("Access-Control-Allow-Origin","*");
-	response.send("hello"+request.params.fname+request.params.lname)
+	MongoClient.connect("mongodb://127.0.0.1:27017",function(err,mongo){
+		if (err == null)
+		{
+			var query = mongo.db("whatifi").collection("users").findOne({"username":request.params.id}, function(err,doc) {
+
+				if (err == null)
+				{
+					response.send(doc);
+				}
+				else
+				{
+					response.send("query error");
+				}
+			});
+		}
+		else
+		{
+			response.send("database error");
+		}
+	});
+});
+
+app.get("/income/:id", function(request,response){
+	console.log("income",request.params);
+	response.header("Access-Control-Allow-Origin","*");
+	MongoClient.connect("mongodb://127.0.0.1:27017",function(err,mongo){
+		if (err == null)
+		{
+			var query = mongo.db("whatifi").collection("income").findOne({"income_id":parseInt(request.params.id)}, function(err,doc) {
+
+				if (err == null)
+				{
+					response.send(doc);
+				}
+				else
+				{
+					response.send("query error");
+				}
+			});
+		}
+		else
+		{
+			response.send("database error");
+		}
+	});
 });
 
 app.listen(8080, () => console.log("listening to port 8080"));
