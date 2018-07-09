777a2a348c5ddbd317a6cfa0e4383066bc975f31
Robert Sanchez <robert.sanchez@sophos.com>
2018-07-08 19:11:24 -0700

    Allow cors

--- node/WFI_Server.js
+++ node/WFI_Server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.get("/:fname/:lname", (request,response) => response.send("hello"+request.params.fname+request.params.lname));
+app.get("/:fname/:lname", function(){
+	response.header("Access-Control-Allow-Origin","*");
+	response.send("hello"+request.params.fname+request.params.lname)
+});
 
 app.listen(8080, () => console.log("listening to port 8080"));
