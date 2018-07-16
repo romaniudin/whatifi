fd9fc94899acc7a534a73adffa6f63717d4703f0
Robert Sanchez <robertsanchez92@gmail.com>
2018-07-15 21:52:46 -0700

    Server supports user upload via JSON file

--- node/WFI_Server.js
+++ node/WFI_Server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 
+app.get("/user/:id", (request,response) => {
 
-app.get("/user/:id", function(request,response){
 	console.log("users",request.params);
 	response.header("Access-Control-Allow-Origin","*");
 	MongoClient.connect("mongodb://127.0.0.1:27017",function(err,mongo){
@@ -28,7 +28,8 @@ app.get("/user/:id", function(request,response){
 	});
 });
 
-app.get("/income/:id", function(request,response){
+app.get("/income/:id", (request,response) => {
+
 	console.log("income",request.params);
 	response.header("Access-Control-Allow-Origin","*");
 	MongoClient.connect("mongodb://127.0.0.1:27017",function(err,mongo){
@@ -52,5 +53,66 @@ app.get("/income/:id", function(request,response){
 		}
 	});
 });
+
+const isValidUserUpload = json => {
+
+	return "username" in json && "email" in json && json["username"] != "" && json["email"] != "";
+}
+
+const saveUser = (json,response) => {
+
+	if (isValidUserUpload(json))
+	{
+		MongoClient.connect("mongodb://127.0.0.1:27017",function(err,mongo){
+			if (err == null)
+			{
+				var query = mongo.db("whatifi").collection("users").insert(json, function(err,doc) {
+
+					if (err == null)
+					{
+						response.send(doc);
+					}
+					else
+					{
+						response.send("insert error");
+					}
+				});
+			}
+			else
+			{
+				response.send("database error");
+			}
+		});
+	}
+	else
+	{
+		response.send("error - missed 'username' and/or 'email'");
+	}
+}
+
+app.post("/user", (request,response) => {
+
+	response.header("Access-Control-Allow-Origin","*");
+	var json = response.body;
+	console.log("recieved user json upload");
+
+	var body = [];
+
+	request.on('data',function(chunk) {
+		body.push(chunk);
+
+	}).on('end', function() {
+		try {
+			var user = JSON.parse(body.toString());
+			saveUser(user,response);
+		}
+		catch (e) {
+			response.statusCode = 400;
+			response.send("Error: Incorrect JSON object - "+e+"\n");
+		}
+	});
+
+
+});	
 
 app.listen(8080, () => console.log("listening to port 8080"));
